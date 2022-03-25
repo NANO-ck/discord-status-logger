4ff0a100d15353a3ff36360d4a4a4ddf01e3bb7f
NANO <66187077+NANO-ck@users.noreply.github.com>
2022-03-25 23:37:32 +0100

    Update watch.js

--- src/watch.js
+++ src/watch.js
@@ -2,13 +2,42 @@
 
 // This file is triggered for each user entered in the config.js
 
-const discord = require("discord.js")
+const { WebhookClient } = require("discord.js")
 const client = require("../index.js")
 const log = require("log.js")
+
+const status = {
+  dnd: {text: "⛔ Do Not Disturb", color: 0xff6600},
+  idle: {text: "🌙 Idle", color: 0xff6600},
+  online: {text: "🟢 Online", color: 0xff6600},
+  offline: {text: "⚫ Offline", color: 0xff6600},
+}
 
 module.exports = async ({id, webhook}) => {
   const user = await client.users.fetch(id)
   if(!user) return log("error", "Couldn't find user with ID " + id)
+  const channel = new WebhookClient(webhook)
+  if(!channel.send({content: "✅ Successfully watching on "+user.tag}).then(() => true).catch(()  => false)) return log("error", "Invalid webhook creditentials for user with ID " + id)
   log("log", "Successfully watching on " + user.tag)
   
+  let status = user.presence.status
+  
+  channel.send({embeds:[{
+    title: status[status].text,
+    description: `Device(s): Object.entries(user.presence.clientStatus).map(x => x[0]).join(", ")`,
+    color: status[status].color,
+    timestamp: Date.now()
+  }]})
+  setInterval(function(){
+      const currentStatus = user.presence.status
+      if(status !== currentStatus) {
+        channel.send({embeds:[{
+          title: status[status].text,
+          description: `Device(s): Object.entries(user.presence.clientStatus).map(x => x[0]).join(", ")`,
+          color: status[status].color,
+          timestamp: Date.now()
+        }]})
+        status = currentStatus        
+      }
+  }, 2500);
 }
