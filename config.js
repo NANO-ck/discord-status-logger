7d1409f098c6922e5934bee9e38ab4341978a3ea
NANO <66187077+NANO-ck@users.noreply.github.com>
2022-03-25 22:11:37 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+
+module.exports = {
+  token: "", // The Discord Bot Token, get it from Discord Developer Portal
+  users: [ // Users you want to monitor
+    {id: "", webhook: ""} // Provide the user ID and a Discord webhook to post messages whenever the user connects or disconnects
+  ]
+}
