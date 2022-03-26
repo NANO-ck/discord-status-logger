ec2e459f34db6cea07ddfb2cc15e2dda38fedc88
NANO <66187077+NANO-ck@users.noreply.github.com>
2022-03-26 16:28:19 +0100

    Update event.js

--- src/event.js
+++ src/event.js
@@ -8,5 +8,10 @@ module.exports = (event, user, channel, status, devices) => {
   if(!event || !user || !user.tag || !channel || !status || !devices) return log("error", "Missing variable in event.js")
   if(!["connect", "disconnect", "statusUpdate", "deviceUpdate"].includes(event)) return log("error", "Incorrect event type in event.js")
   
-  
+  channel.send({embeds:[{
+          title: status[status].text,
+          description: `Device(s): Object.entries(member.presence.clientStatus).map(x => x[0]).join(", ")`,
+          color: status[status].color,
+          timestamp: Date.now()
+        }]})
 }
