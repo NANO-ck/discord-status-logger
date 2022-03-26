172746c5a7e587c1c28419ff2956481b8d7ee0a0
NANO <66187077+NANO-ck@users.noreply.github.com>
2022-03-26 16:19:54 +0100

    Update event.js

--- src/event.js
+++ src/event.js
@@ -1,12 +1,12 @@
 // src/event.js
 
-// This file is triggered whenever a user has an event (connect, disconnect, or deviceUpdate)
+// This file is triggered whenever a user has an event (connect, disconnect, statusUpdate, or deviceUpdate)
 
 const log = require("./log.js")
 
 module.exports = (event, user, channel, status, devices) => {
   if(!event || !user || !user.tag || !channel || !status || !devices) return log("error", "Missing variable in event.js")
-  if(!["connect", "disconnect", "deviceUpdate"].includes(event)) return log("error", "Incorrect event type in event.js")
+  if(!["connect", "disconnect", "statusUpdate", "deviceUpdate"].includes(event)) return log("error", "Incorrect event type in event.js")
   
   
 }
